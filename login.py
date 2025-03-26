5f19501c1bf9ac950596557788a0992c2c7ed49e
Rafael Mandel <rafa80744@gmail.com>
2025-03-26 17:09:35 -0300

    feat - login finalizado parcialmente

--- login.py
+++ login.py
@@ -2,17 +2,46 @@
 
 #login de verificacao dos medicos
 logins = {
-    'id_medico_1': {'Nome': 'Nome Medico', 'CRM': '', 'senha': 'senha','hierarquia': '','Area': '', 'CPF': 'cpf sem espaco e simbolos', }
+    'id_medico_1': {
+        'Nome': 'Nome Medico', 'CRM': '', 'senha': 'senha','hierarquia': '','Area': '', 'CPF': 'cpf sem espaco e simbolos', 
+        }
     }
 
+def forca_opcao(msg, opcao_validas, msg2):
+    while True:
+        escolha = input(msg).strip()
+        if escolha in opcoes_validas:
+            return escolha
+        print(msg2)
+
 def login_medico():
-    pedir_id = input("Digite sua identificacao: ") #colocar uma def forcar_opcao
-    #retornar uma validacao para que tenha acesso ao sistema
-    #provavelmente vai retornar a funcao main que vai ser a que vai ter tudo do sistema :)
-    if pedir_id == logins[] #fazer com que essa funcao verifique os IDs e se for um cadastrado peca a senha
-        pedir_senha = input("Digite sua senha: ") #colocar def forcar_opcao
-        #retornar que caso a senha esteja certa ele se conecta no sistema e abre a def main() para ter acesso ao sistema
-        
-    #desde o comeco da def, ter a opcao de voltar para caso tenha trocado o id ou ter sido acertado na sorte
+    while True:
+        pedir_id = input("Digite sua identificacao (ou sair para voltar): ").strip()
+        if pedir_login.lower() == 'sair':
+            return False
+        if pedir_id == logins:
+            pedir_senha = input("Digitre sua senha: ").strip()
+            if pedir_senha == login[pedir_id]['senha']:
+                print(f"Login bem-sucedido! Bem vindo {login[pedir_id]}")
+                return True 
+            else:
+                print("Senha incorreta. Tente Novamente.")
+        else:
+            print("ID nao encontrado. Tente novamente.")
+
+def registrar_medico():
+    novo_id = input("Digite um novo ID para o medico: ")
+    if novo_id in logins:
+        print("Este ID ja esta em uso. Escolha outro.")
+        return
+    nome = input("Digite o nome do medico: ").strip()
+    crm = input("Digite o CRM: ").strip()
+    senha = input("Digite a senha: ").strip()
+    hierarquia = input("Digite a hierarquia: ").strip()
+    area = intup("Digite a area de atuacao: ").strip()
+    cpf = input("Digite o CPF (sem espacos ou simbolos: )").strip()
     
-#agora seguindo essa logica fazer uma def para registrar os medicos     
+    logins[novo_id] = {
+        'Nome': nome, 'CRM': crm, 'senha': senha, 'hierarquia': hierarquia, 'Area': area, 'CPF': cpf
+    }
+    print("Medico cadastrado no sistema!")
