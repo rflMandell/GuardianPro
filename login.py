799965ea8cdf53f9fba9e76f083d5b69c7af71c6
Rafael Mandel <rafa80744@gmail.com>
2025-03-26 15:51:02 -0300

    feat - criacao do sistema de login dos medicos

---
+++ login.py
@@ -0,0 +1,18 @@
+#fazer algumas partes separado para ir fazendo aos pouquinhos, ai quando terminar juntar com no main.py
+
+#login de verificacao dos medicos
+logins = {
+    'id_medico_1': {'Nome': 'Nome Medico', 'CRM': '', 'senha': 'senha','hierarquia': '','Area': '', 'CPF': 'cpf sem espaco e simbolos', }
+    }
+
+def login_medico():
+    pedir_id = input("Digite sua identificacao: ") #colocar uma def forcar_opcao
+    #retornar uma validacao para que tenha acesso ao sistema
+    #provavelmente vai retornar a funcao main que vai ser a que vai ter tudo do sistema :)
+    if pedir_id == logins[] #fazer com que essa funcao verifique os IDs e se for um cadastrado peca a senha
+        pedir_senha = input("Digite sua senha: ") #colocar def forcar_opcao
+        #retornar que caso a senha esteja certa ele se conecta no sistema e abre a def main() para ter acesso ao sistema
+        
+    #desde o comeco da def, ter a opcao de voltar para caso tenha trocado o id ou ter sido acertado na sorte
+    
+#agora seguindo essa logica fazer uma def para registrar os medicos 
