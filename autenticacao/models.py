84bb7b61e16ea6179c22866226d51821dfefba06
Rafael Mandel <rafa80744@gmail.com>
2025-04-04 05:46:04 -0300

    feat - django and migrate manage.py

---
+++ autenticacao/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+
+class Usuario(AbstractUser):
+    cargo = models.CharField(max_length=50, choices=[
+        ('medico', 'Medico'),
+        ('admin', 'Administrador'),
+    ], default='medico')
