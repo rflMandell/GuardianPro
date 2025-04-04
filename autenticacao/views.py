326c235819cc75bb9d74c74154280b56914bcbaf
Rafael Mandel <rafa80744@gmail.com>
2025-04-04 06:03:31 -0300

    feat - creating vews and templates
    criando as views para processar os logins e registros

--- autenticacao/views.py
+++ autenticacao/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from .forms import RegistroForm, LoginForm
 
 # Create your views here.
