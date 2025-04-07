3964921ac4a3970db6b083947fc9da4afe487cd6
Rafael Mandel <rafa80744@gmail.com>
2025-04-07 11:18:51 -0300

    feat - sistema de login praticamente finalizado

--- autenticacao/views.py
+++ autenticacao/views.py
@@ -3,3 +3,30 @@ from django.contrib.auth import authenticate, login, logout
 from .forms import RegistroForm, LoginForm
 
 # Create your views here.
+def registrar_usuario(request):
+    if request.method == "POST":
+        form = RegistroForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = RegistroForm()
+    return render(request, 'autenticacao/registro.html', {'form': form})
+
+def login_usuario(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            if user:
+                login(request, user)
+                return redirect('home')
+    else:
+        form = LoginForm()
+    return render(request, 'autenticacao/login.html', {'form': form})
+
+def logout_usuario(request):
+    logout(request)
+    return redirect('login')
