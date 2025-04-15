e358358ea12ff522a8fa817ea4e8d76063e700e6
Rafael Mandel <rafa80744@gmail.com>
2025-04-15 16:14:38 -0300

    fix - makemigrations error

--- chamadas/admin.py
+++ chamadas/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Chamada
 
-# Register your models here.
+@admin.register(Chamada)
+class ChamadaAdmin(admin.ModelAdmin):
+    list_display = ('canal', 'uid_medico', 'uid_paciente', 'inicio', 'fim')
+    search_fields = ('canal', 'uid_medico', 'uid_paciente')
+    list_filter = ('inicio', 'fim')
--- chamadas/models.py
+++ chamadas/models.py
@@ -1,23 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
-
-class Gravacao(models.Model)Ç
+class Gravacao(models.Model):
     canal = models.CharField(max_length=100)
     uid_medico = models.CharField(max_length=100)
     uid_paciente = models.CharField(max_length=100)
-    timestamp_incio = models.DateTimeField()
+    timestamp_inicio = models.DateTimeField()
     timestamp_fim = models.DateTimeField()
     criado_em = models.DateTimeField(auto_now_add=True)
-    
+
     medico = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name='gravacoes_medico'
     )
     paciente = models.ForeignKey(
-        User, on_delete=models.CASCADE,
-        related_name='gravacoes_paciente'
+        User, on_delete=models.CASCADE, related_name='gravacoes_paciente'
     )
-    
+
     def __str__(self):
-        return f"Chamada {self.canal} - {self.timestamp_inicio.strtime('%d/%m/%Y %H:%M')}"
+        return f"Chamada {self.canal} - {self.timestamp_inicio.strftime('%d/%m/%Y %H:%M')}"
