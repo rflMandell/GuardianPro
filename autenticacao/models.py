99c14a0f29edc56a01e7506f7d699827cb1e2adf
Rafael Mandel <rafa80744@gmail.com>
2025-06-13 16:54:29 -0300

    fix - bug in auth

--- autenticacao/models.py
+++ autenticacao/models.py
@@ -1,10 +1,27 @@
+from django.contrib.auth.models import AbstractUser, Group, Permission
 from django.db import models
-from django.contrib.auth.models import AbstractUser
-
-# Create your models here.
+from django.utils.translation import gettext_lazy as _
 
 class Usuario(AbstractUser):
-    cargo = models.CharField(max_length=50, choices=[
-        ('medico', 'Medico'),
-        ('admin', 'Administrador'),
-    ], default='medico')
+    groups = models.ManyToManyField(
+        Group,
+        verbose_name=_('groups'),
+        blank=True,
+        help_text=_(
+            'The groups this user belongs to. A user will get all permissions '
+            'granted to each of their groups.'
+        ),
+        related_name="usuario_set_groups",  # Nome único para o related_name
+        related_query_name="usuario",
+    )
+    user_permissions = models.ManyToManyField(
+        Permission,
+        verbose_name=_('user permissions'),
+        blank=True,
+        help_text=_('Specific permissions for this user.'),
+        related_name="usuario_set_permissions",  # Nome único para o related_name
+        related_query_name="usuario",
+    )
+
+    def __str__(self):
+        return self.username
