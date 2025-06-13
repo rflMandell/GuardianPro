c7278b255f3dad63d17c74413bd0d367ca9ce6d9
Rafael Mandel <rafa80744@gmail.com>
2025-06-13 17:02:00 -0300

    Update Readme.md

--- laudos_ia/urls.py
+++ laudos_ia/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 
-app_name = 'laudos_ia'
+app_name = 'laudos_ia' 
 
 urlpatterns = [
     path('', views.listar_laudos_pendentes, name='listar_laudos_pendentes'),
